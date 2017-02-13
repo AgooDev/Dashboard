5d01a2129e247b61c47ba546c80729f43c72b622
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-12 23:55:11 -0500

    temporal nodejs static files server

---
+++ server.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2017-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(3000);
+});
+console.log('Server running at http://localhost:3000/');
