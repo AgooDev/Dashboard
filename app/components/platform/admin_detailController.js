695069d3c4b6a408a66ba859bc9de5af2c6b7825
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-02-19 00:47:51 -0500

    admin routes, views and controllers

---
+++ app/components/platform/admin_detailController.js
@@ -0,0 +1,15 @@
+angular
+    .module('agooDash')
+    .controller('admin_detailCtrl', [
+        '$rootScope',
+        '$scope',
+        'user_data',
+        function ($rootScope,$scope,user_data) {
+
+            $scope.user_data = user_data[0];
+
+            $scope.user_data_contacts = user_data[0].contact;
+
+        }
+    ])
+;
