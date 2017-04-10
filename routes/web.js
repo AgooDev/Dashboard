6165f787d1530f9e3b3a43510822651f690464ec
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-04-10 02:06:55 -0500

    general web routes

--- routes/web.js
+++ routes/web.js
@@ -5,3 +5,42 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree or translated in the assets folder.
  */
+
+/**
+ * Module dependencies
+ */
+var logger = require('../utils/logger').logger;
+var moment = require('moment');
+var express = require('express');
+var router = express.Router();
+
+/**
+ * SetupWebRouter
+ *
+ * @description  Configure all routes on express router
+ *
+ * @param {express}      app      The application server
+ */
+function SetupWebRouter(app) {
+    // logger for all request will first hits this middleware
+    router.use(function (req, res, next) {
+        var now = moment(new Date());
+
+        var date = now.format('DD-MM-YYYY HH:mm');
+        logger.info('%s %s %s', req.method, req.url, date);
+        next();
+    });
+
+    app.use(router);
+
+    /**
+     *  Declare all routes
+     */
+    var landingRoutes = require('./landing');
+
+    app.use('/', landingRoutes);
+
+}
+
+// Export setup function
+module.exports.SetupWebRouter = SetupWebRouter;
